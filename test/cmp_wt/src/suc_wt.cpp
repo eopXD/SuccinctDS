ac07895cd015cbb058290187e64664d1f29cceae
eopXD <eopxd@ntu.im>
2019-07-27 14:40:18 +0800

    functions properly

--- test/cmp_wt/src/suc_wt.cpp
+++ test/cmp_wt/src/suc_wt.cpp
@@ -6,23 +6,22 @@
 */
 
 #include <cstdio>
+#include <cstring>
 #include <cstdlib>
 #include <ctime>
 #include <stdint.h>
 #include <cassert>
 #include <iostream>
-#include <string>
 
+#include "include/util.hpp"
 #include "include/wt.hpp"
 using namespace eopxd;
 
 typedef uint64_t INT;
-const INT filesize = 2000000000;
-char a, b; // calculate rank on 2 random characters
-//std::string input; // reading the whole string to assert access correct
+unsigned char a, b; // calculate rank on 2 random characters
 INT *rank[2];
 int cnt[2], now;
-int main ()
+int main ( int argc, char *argv[] )
 {
 	clock_t start = clock(), stamp = clock();
 
@@ -30,27 +29,30 @@ int main ()
 	a = rand()%256, b = rand()%256;
 
 	char filename[25] = "test_data.big";
-	int bpa = 1;
+	int name_len = strlen(filename);
+	INT filesize = get_size(filename);	
+	std::cout << "filesize: " << filesize << "\n";
+
+	int bpa = 1, dummy = 1;
 	wt<wt_huff<bv_lookup>, bv_lookup> *wt_ptr = 
-		new wt<wt_huff<bv_lookup>, bv_lookup>(filename, bpa, 1); 
-	std::cout << wt_ptr->rank_support << "\n";
+		new wt<wt_huff<bv_lookup>, bv_lookup>(filename, name_len, bpa, dummy); 
 	
 	std::cout << "initialize: " << spent_time(stamp) << " seconds\n";
 	stamp = clock();
-	
+
 	rank[0] = new INT [filesize+1];
 	rank[1] = new INT [filesize+1];
 	rank[0][0] = 0, rank[1][0] = 0;
 	cnt[0] = cnt[1] = 0;
 	now = 1;
-		
+
 	int fd = open(filename, O_RDONLY);
 	if ( fd < 0 ) {
 		std::cout << "open file fail\n";
 		return (0);
 	}
 	INT buf_size = bpa*65536, sz;
-	char *buf = new char [buf_size];
+	unsigned char *buf = new unsigned char [buf_size];
 	while ( (sz=read(fd, buf, buf_size)) > 0 ) {
 		for ( INT i=0; i<sz; ++i ) {
 			if ( buf[i] == a ) cnt[0]++;
@@ -59,9 +61,7 @@ int main ()
 			rank[1][now++] = cnt[1];
 		}	
 	}
-	close(fd);
-	std::cout << "now: " << now << "\n";
-	
+	close(fd);	
 	std::cout << "naive rank: " << spent_time(stamp) << " seconds\n";
 	stamp = clock();
 
@@ -69,11 +69,9 @@ int main ()
 	std::cout << "support rank: " << spent_time(stamp) << " seconds\n";
 	stamp = clock();
 
-
-	std::string s, t;
-	s += a, t+= b;
 	INT times = 100000000;
-	INT twice = times*2, fourth = times*4, eighth = times*8;
+	INT twice = times*2, fourth = times*4;
+/* access operation */
 	for ( INT i=0; i<times; ++i ) {
 		INT pos = rand()%filesize;
 		wt_ptr->access(pos);
@@ -94,18 +92,11 @@ int main ()
 	}
 	std::cout << "access " << fourth << " times, " << spent_time(stamp) << " seconds\n";
 	stamp = clock();
-
-	for ( INT i=0; i<eighth; ++i ) {
-		INT pos = rand()%filesize;
-		wt_ptr->access(pos);
-	}
-	std::cout << "access " << eighth << " times, " << spent_time(stamp) << " seconds\n";
-	
-	stamp = clock();
+/* rank operation */
 	for ( INT i=0; i<times; ++i ) {
 		INT pos = rand()%filesize;
-		INT r0 = wt_ptr->rank(s, pos);
-		INT r1 = wt_ptr->rank(t, pos);
+		INT r0 = wt_ptr->rank(a, pos);
+		INT r1 = wt_ptr->rank(b, pos);
 
 		assert(r0 == rank[0][pos]);
 		assert(r1 == rank[1][pos]);
@@ -115,8 +106,8 @@ int main ()
 
 	for ( INT i=0; i<twice; ++i ) {
 		INT pos = rand()%filesize;
-		INT r0 = wt_ptr->rank(s, pos);
-		INT r1 = wt_ptr->rank(t, pos);
+		INT r0 = wt_ptr->rank(a, pos);
+		INT r1 = wt_ptr->rank(b, pos);
 
 		assert(r0 == rank[0][pos]);
 		assert(r1 == rank[1][pos]);
@@ -125,23 +116,13 @@ int main ()
 	stamp = clock();
 	for ( INT i=0; i<fourth; ++i ) {
 		INT pos = rand()%filesize;
-		INT r0 = wt_ptr->rank(s, pos);
-		INT r1 = wt_ptr->rank(t, pos);
+		INT r0 = wt_ptr->rank(a, pos);
+		INT r1 = wt_ptr->rank(b, pos);
 
 		assert(r0 == rank[0][pos]);
 		assert(r1 == rank[1][pos]);
 	}
 	std::cout << "rank " << fourth << " times, " << spent_time(stamp) << " seconds\n";
-	stamp = clock();
-	for ( INT i=0; i<eighth; ++i ) {
-		INT pos = rand()%filesize;
-		INT r0 = wt_ptr->rank(s, pos);
-		INT r1 = wt_ptr->rank(t, pos);
-
-		assert(r0 == rank[0][pos]);
-		assert(r1 == rank[1][pos]);
-	}
-	std::cout << "rank " << eighth << " times, " << spent_time(stamp) << " seconds\n";
 	stamp = clock();
 
 	std::cout << "total: " << spent_time(start) << " seconds\n";
