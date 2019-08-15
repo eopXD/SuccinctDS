ab687e15a85fab242e17da23539f4a4ac48d4abf
Yueh-Ting Chen <eopXD@white.iis.sinica.edu.tw>
2019-08-15 23:44:20 +0800

    try compress program to see experiment compression results

--- investigate/src/try_compress.cpp
+++ investigate/src/try_compress.cpp
@@ -17,34 +17,38 @@
 #include "include/wt.hpp"
 
 using namespace eopxd;
-
+typedef uint64_t UINT64;
+typedef wt<wt_huff<bv_lookup>, bv_lookup> WAVELET;
 int main ()
 {
 	char filename[100];
 	int namelen;
 	int bpa = 1, dummy = 1;
-	wt<wt_huff<bv_lookup>, bv_lookup> *wt_ptr;
-	// decode0
-	for ( int i=0; i<30; ++i ) {
-		sprintf(filename, "/tmp2/b04705001/4x5_result_decoded1/part%03d.dat", i);
-		puts(filename);
+	WAVELET *wt_ptr;
+	while ( 1 ) {
+		std::cout << "input filename (absolute path): ";
+		std::cin >> filename;
 		namelen = strlen(filename);
-		wt_ptr = new wt<wt_huff<bv_lookup>, bv_lookup>(filename, namelen, bpa, dummy);
-
-		std::cout << "mem_used: " << wt_ptr->mem_used << "\n";
-		delete wt_ptr;
-		star_line();
-		//break;
-	}
-	// decode1
-	/*for ( int i=0; i<30; ++i ) {
-		sprintf(filename, "/tmp2/b04705001/4x5_result_decoded1/part%03d.dat", i);
+		UINT64 file_size = get_filesize(filename);
+		std::cout << "\n\nfile: " << filename << "\n";
+		std::cout << "filesize: " << file_size << " " << (double)file_size/(1LL<<30) << "G\n";
+	
+		wt_ptr = new WAVELET(filename, namelen, bpa, dummy);
 		
+		std::cout << "\n\nwavelet tree:\n";
+		std::cout << "mem_used: :" << wt_ptr->mem_used << " " << 
+			(double)wt_ptr->mem_used/(1LL<<30) << "G\n";
+		std::cout << "ratio: " << (double)wt_ptr->mem_used/file_size << "\n"; 
+		
+		wt_ptr->support_rank();
+		
+		std::cout << "\nafter support_rank():\n";
+		std::cout << "mem_used: :" << wt_ptr->mem_used << " " << 
+			(double)wt_ptr->mem_used/(1LL<<30) << "G\n";
+		std::cout << "ratio: " << (double)wt_ptr->mem_used/file_size << "\n"; 
 
-
-
-		break;
-	}*/
+		delete wt_ptr;
+	}
 	return (0);
 
 }
