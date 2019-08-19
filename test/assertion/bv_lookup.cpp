026907c7cc47a4c3f07d7fb452f41858cd149f0f
Yueh-Ting Chen <eopXD@white.iis.sinica.edu.tw>
2019-08-19 21:03:16 +0800

    adjustment, use assign()/assess() to interact with bv_lookup before calling support_rank()

--- test/assertion/bv_lookup.cpp
+++ test/assertion/bv_lookup.cpp
@@ -15,45 +15,44 @@
 
 using namespace eopxd;
 typedef uint64_t INT;
-
+typedef bv_lookup BV;
 int main ()
 {
-	int n = 1000000, m = 500;
-	bv_lookup *bv_ptr = new bv_lookup(n);
-	bv_lookup bv_var(n);
+	int n = 100000000, m = 500;
+	
+	init_bv_lookup();
+	
+	BV *bv_ptr = new BV(n);
 	bool *ans = new bool [n];
 	INT *naive_rank = new INT [n+1];
 	for ( int i=0; i<n; ++i ) {
-		bv_ptr->bitvec[i] = bv_var.bitvec[i] = ans[i] = 0;
+		bv_ptr->assign(i, 0);		
+		ans[i] = 0;
 	}
 	for ( int i=0; i<m; ++i ) {
 		int x = rand()%n;
-		bv_ptr->bitvec[x] = bv_var.bitvec[x] = ans[x] = 1;
+		bv_ptr->assign(x, 1);
+		ans[x] = 1;
 	}
 	//for ( int i=0; i<n; ++ i ) printf("%d", ans[i]); puts("");
 	naive_rank[0] = 0;
 	for ( int i=0; i<n; ++i ) {
 		naive_rank[i+1] = naive_rank[i]+ans[i];
-		assert(bv_ptr->bitvec[i] == ans[i]);
-		assert(bv_var.bitvec[i] == ans[i]);
+		assert(bv_ptr->access(i) == ans[i]);
 		
 	}
 
+	std::cout << "before support_rank()\n";
+	std::cout << "bv_ptr memory: " << bv_ptr->mem_used << "\n";
 	bv_ptr->support_rank();
-	bv_var.support_rank();
-	//bv_ptr->display_rank();
+	std::cout << "after support_rank()\n";
+	std::cout << "bv_ptr memory: " << bv_ptr->mem_used << "\n";
 	
-	//for ( int i=0; i<15; ++i ) {
-	//	printf("%2d: %d, %d\n", i, vec->access(i), ans[i]);
-	//}
 	for ( int i=0; i<n; ++i ) {
 		assert(bv_ptr->access(i) == ans[i]);
 		assert(bv_ptr->rank(i, 0) == (i-naive_rank[i]));
 		assert(bv_ptr->rank(i, 1) == naive_rank[i]);
-
-		assert(bv_var.access(i) == ans[i]);
-		assert(bv_var.rank(i, 0) == (i-naive_rank[i]));
-		assert(bv_var.rank(i, 1) == naive_rank[i]);
 	}
+	std::cout << "pass assertion (access/rank)\n";
 	exit(0);
 }
