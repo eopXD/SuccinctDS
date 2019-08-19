af3b17c541fceac343fcac88d82bdd7c3841bc34
Yueh-Ting Chen <eopXD@white.iis.sinica.edu.tw>
2019-08-19 21:36:37 +0800

    further investigation needed

--- test/assertion/mem_used_wt.cpp
+++ test/assertion/mem_used_wt.cpp
@@ -15,20 +15,29 @@
 #include "include/wt.hpp"
 using namespace eopxd;
 
-typedef uint64_t INT;
+typedef uint64_t UINT64;
 typedef unsigned char UCHAR;
 
 UCHAR test_string[10] = "aaaabbaac";
 int main ()
 {
-	INT n = strlen((char*)test_string);
+	UINT64 n = strlen((char*)test_string);
 	int bpa = 1;
 	wt<wt_huff<bv_lookup>, bv_lookup> *wt_ptr = 
 		new wt<wt_huff<bv_lookup>, bv_lookup>(test_string, n, bpa);  		
+	
+	UINT64 before_support;
+	
+	std::cout << "before support_rank()\n";
 	std::cout << "memory_used: " << wt_ptr->mem_used << "\n";
+	before_support = wt_ptr->mem_used;
+
 	wt_ptr->support_rank();
+	
+	std::cout << "after support_rank()\n";
 	std::cout << "memory_used: " << wt_ptr->mem_used << "\n";
 
+	std::cout << "\nratio: " << (double)wt_ptr->mem_used/before_support << "\n";
 
 	return (0);
 }
